dd566aa37dc7091fd5e7b588d991d89077c1e20b
Rydog101 <ryanjeastland@gmail.com>
2015-07-18 21:17:38 +1000

    Got NBT tag copying working

--- src/main/java/net/rystuff/hostagemod/HostageMod.java
+++ src/main/java/net/rystuff/hostagemod/HostageMod.java
@@ -1,5 +1,6 @@
 package net.rystuff.hostagemod;
 
+import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
@@ -7,15 +8,21 @@ import cpw.mods.fml.common.registry.EntityRegistry;
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemSaddle;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.CraftingManager;
+import net.minecraft.item.crafting.ShapelessRecipes;
 import net.minecraftforge.common.MinecraftForge;
 import net.rystuff.hostagemod.blocks.BlockChair;
 import net.rystuff.hostagemod.entity.EntityMountableBlock;
 import net.rystuff.hostagemod.event.EventChairRope;
 import net.rystuff.hostagemod.event.EventGag;
 import net.rystuff.hostagemod.event.EventHandcuff;
+import net.rystuff.hostagemod.event.EventHandler;
 import net.rystuff.hostagemod.item.*;
 import net.rystuff.hostagemod.proxy.ServerProxy;
+
+import java.util.ArrayList;
 
 @Mod(modid = "hostagemod", name = "Hostage Mod", version = "0.1.2")
 public class HostageMod
@@ -36,7 +43,7 @@ public class HostageMod
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event)
     {
-        // MinecraftForge.EVENT_BUS.register(new EventHandler());
+        FMLCommonHandler.instance().bus().register(new EventHandler());
         MinecraftForge.EVENT_BUS.register(new EventGag());
         MinecraftForge.EVENT_BUS.register(new EventHandcuff());
         MinecraftForge.EVENT_BUS.register(new EventChairRope());
@@ -46,6 +53,8 @@ public class HostageMod
         GameRegistry.registerItem(key, "key");
         GameRegistry.registerItem(lock, "lock");
         GameRegistry.addShapelessRecipe(new ItemStack(key, 1), new ItemStack(key));
+        GameRegistry.addShapelessRecipe(new ItemStack(handcuffs, 1), new ItemStack(handcuffs));
+        GameRegistry.addShapelessRecipe(new ItemStack(handcuffs, 1), new ItemStack(key), new ItemStack(handcuffs));
         int entityId = EntityRegistry.findGlobalUniqueEntityId();
         EntityRegistry.registerGlobalEntityID(EntityMountableBlock.class, "mountableBlock", entityId);
         EntityRegistry.registerModEntity(EntityMountableBlock.class, "mountableBlock", entityId, HostageMod.instance, 64, 1, true);
--- src/main/java/net/rystuff/hostagemod/event/EventHandler.java
+++ src/main/java/net/rystuff/hostagemod/event/EventHandler.java
@@ -1,57 +1,37 @@
 package net.rystuff.hostagemod.event;
 
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.common.gameevent.*;
+import cpw.mods.fml.common.gameevent.PlayerEvent;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraftforge.event.entity.living.EnderTeleportEvent;
 import net.minecraftforge.event.entity.player.*;
 import net.minecraftforge.event.world.BlockEvent;
+import net.rystuff.hostagemod.HostageMod;
 
 public class EventHandler
 {
     @SubscribeEvent
-    public void attackEntity(AttackEntityEvent event)
+    public void crafted(PlayerEvent.ItemCraftedEvent event)
     {
-        event.setCanceled(true);
-    }
-
-    @SubscribeEvent
-    public void blockBreak(BlockEvent.BreakEvent event)
-    {
-        event.setCanceled(true);
-    }
-
-    @SubscribeEvent
-    public void blockPlace(BlockEvent.PlaceEvent event)
-    {
-        event.setCanceled(true);
-    }
-
-    @SubscribeEvent
-    public void itemuse(PlayerUseItemEvent.Start event)
-    {
-        event.setCanceled(true);
-    }
-
-    @SubscribeEvent
-    public void playerInteract(PlayerInteractEvent event)
-    {
-        event.setCanceled(true);
-    }
-
-    @SubscribeEvent
-    public void enderTeleport(EnderTeleportEvent event)
-    {
-        event.setCanceled(true);
-    }
-
-    @SubscribeEvent
-    public void itemPickup(EntityItemPickupEvent event)
-    {
-        event.setCanceled(true);
-    }
-
-    @SubscribeEvent
-    public void xpPickup(PlayerPickupXpEvent event)
-    {
-        event.setCanceled(true);
+        if (event.crafting.getItem() == HostageMod.handcuffs)
+        {
+            for (int i=0; i < event.craftMatrix.getSizeInventory(); i++)
+            {
+                if (event.craftMatrix.getStackInSlot(i) != null)
+                {
+                   if (event.craftMatrix.getStackInSlot(i).getItem() == HostageMod.key)
+                   {
+                       String keyOwner = event.craftMatrix.getStackInSlot(i).stackTagCompound.getString("owner");
+                       event.crafting.stackTagCompound = new NBTTagCompound();
+                       event.crafting.stackTagCompound.setString("owner", keyOwner);
+                       int keyCode = event.craftMatrix.getStackInSlot(i).stackTagCompound.getInteger("code");
+                       event.crafting.stackTagCompound.setInteger("code", keyCode);
+                       System.out.println("DONE!!!!");
+                   }
+                }
+            }
+        }
     }
 }
